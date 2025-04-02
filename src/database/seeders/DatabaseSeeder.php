42ecc528121bc36bfcea0f45dc69d5a121f43588
y_has <yuko1640@gmail.com>
2025-04-02 16:12:02 +0900

    リモートの変更

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
        $this->call(ProductsTableSeeder::class);
        $this->call(SeasonsTableSeeder::class);
+       $this->call(ProductSeasonTableSeeder::class);
     }
 }
